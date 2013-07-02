6056c5a373c502c6a32fa7da903593c68434d9a0
Alex Wolkov <altryne@gmail.com>
2013-07-02 10:45:25 +0300

    updated a bit

--- js/quicktimer.js
+++ js/quicktimer.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     var duration = ""
     duration = (location.pathname)? location.pathname.substr(1) : "";
-    var parsed = duration.split(/(\d+.)?/)
+    var parsed = duration.split(/(\d+[\D]+)/)
     var end_time = moment();
     $.each(parsed,function(i,item){
         if (item == "") return;
@@ -13,13 +13,6 @@ $(document).ready(function(){
     })
     console.log(end_time.fromNow());
 
-//    setInterval(function(){
-//        real_now = moment()
-//        diff = now.diff(real_now,'seconds')
-//        console.log(diff);
-//    },1000)
-
-
     // the date when the countdown has to end yyyy, mm(starting from 0 not from 1), dd, hh, mm, ss
     // for months 0 == january, 1 == february
     var countdown = $('#countdown'),
@@ -29,8 +22,24 @@ $(document).ready(function(){
         seconds = countdown.find('.seconds span'),
         set_count_down, time_loop;
 
-    set_count_down = function () {
 
+    /* prepare canvas */
+    canvas=document.getElementById('mycanvas');
+    ctx=canvas.getContext('2d');
+    cWidth=canvas.width;
+    cHeight=canvas.height;
+
+    countTo = Math.ceil(end_time.diff(moment()) / 1000);
+
+    min=Math.floor(countTo/60);
+    sec=countTo-(min*60);
+    counter=0;
+    angle=270;
+    inc=360/countTo;
+
+    drawScreen()
+    set_count_down = function () {
+        drawScreen()
         var now = moment(),
             time_left = end_time.diff(now) / 1000,
             d, h, m;
@@ -61,3 +70,134 @@ $(document).ready(function(){
     set_count_down()
     time_loop = setInterval(set_count_down, 1000);
 })
+
+/*===============================
+
+Countdown.
+Based on Kerem Suer dribble shot:
+http://dribbble.com/shots/560534
+
+change value of the variable --countTo-- to set the timer.
+Would love to see someone adding a UI to this one.
+
+=================================*/
+
+  function drawScreen() {
+
+
+
+    //======= reset canvas
+
+    ctx.fillStyle="#2e3032";
+    ctx.fillRect(0,0,cWidth,cHeight);
+
+    //========== base arc
+
+    ctx.beginPath();
+    ctx.strokeStyle="#252424";
+    ctx.lineWidth=14;
+    ctx.arc(cWidth/2,cHeight/2,100,(Math.PI/180)*0,(Math.PI/180)*360,false);
+    ctx.stroke();
+    ctx.closePath();
+
+    //========== dynamic arc
+
+    ctx.beginPath();
+    ctx.strokeStyle="#df8209";
+    ctx.lineWidth=14;
+    ctx.arc(cWidth/2,cHeight/2,100,(Math.PI/180)*270,(Math.PI/180)*angle,false);
+    ctx.stroke();
+    ctx.closePath();
+
+    //======== inner shadow arc
+
+    grad=ctx.createRadialGradient(cWidth/2,cHeight/2,80,cWidth/2,cHeight/2,115);
+    grad.addColorStop(0.0,'rgba(0,0,0,.4)');
+    grad.addColorStop(0.5,'rgba(0,0,0,0)');
+    grad.addColorStop(1.0,'rgba(0,0,0,0.4)');
+
+    ctx.beginPath();
+    ctx.strokeStyle=grad;
+    ctx.lineWidth=14;
+    ctx.arc(cWidth/2,cHeight/2,100,(Math.PI/180)*0,(Math.PI/180)*360,false);
+    ctx.stroke();
+    ctx.closePath();
+
+    //======== bevel arc
+
+    grad=ctx.createLinearGradient(cWidth/2,0,cWidth/2,cHeight);
+    grad.addColorStop(0.0,'#6c6f72');
+    grad.addColorStop(0.5,'#252424');
+
+    ctx.beginPath();
+    ctx.strokeStyle=grad;
+    ctx.lineWidth=1;
+    ctx.arc(cWidth/2,cHeight/2,93,(Math.PI/180)*0,(Math.PI/180)*360,true);
+    ctx.stroke();
+    ctx.closePath();
+
+    //====== emboss arc
+
+    grad=ctx.createLinearGradient(cWidth/2,0,cWidth/2,cHeight);
+    grad.addColorStop(0.0,'transparent');
+    grad.addColorStop(0.98,'#6c6f72');
+
+    ctx.beginPath();
+    ctx.strokeStyle=grad;
+    ctx.lineWidth=1;
+    ctx.arc(cWidth/2,cHeight/2,107,(Math.PI/180)*0,(Math.PI/180)*360,true);
+    ctx.stroke();
+    ctx.closePath();
+
+    //====== Labels
+
+    var textColor='#646464';
+    var textSize="12";
+    var fontFace="helvetica, arial, sans-serif";
+
+    ctx.fillStyle=textColor;
+    ctx.font=textSize+"px "+fontFace;
+    ctx.fillText('MIN',cWidth/2-46,cHeight/2-40);
+    ctx.fillText('SEC',cWidth/2+25,cHeight/2-15);
+
+    //====== Values
+
+
+
+    ctx.fillStyle='#6292ae';
+
+    if (min>9) {
+      ctx.font='84px '+fontFace;
+      ctx.fillText('9' ,cWidth/2-55,cHeight/2+35);
+
+      ctx.font='24px '+fontFace;
+      ctx.fillText('+' ,cWidth/2-72,cHeight/2-5);
+    }
+    else {
+      ctx.font='84px '+fontFace;
+      ctx.fillText(min ,cWidth/2-60,cHeight/2+35);
+    }
+
+    ctx.font='50px '+fontFace;
+    if (sec<10) {
+      ctx.fillText('0'+sec,cWidth/2+10,cHeight/2+35);
+    }
+    else {
+      ctx.fillText(sec,cWidth/2+10,cHeight/2+35);
+    }
+
+
+    if (sec<=0 && counter<countTo) {
+      min--;
+      sec=59;
+    } else
+      if (counter>=countTo) {
+        sec=0;
+        min=0;
+        angle=-90;
+      } else {
+        angle+=inc;
+        counter++;
+        sec--;
+      }
+  }
